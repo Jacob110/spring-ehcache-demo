678df6d8f6d4693d71153412370660704e8ca1fa
JJ_Jacob <yjj.jacob@gmail.com>
2014-11-21 16:51:40 +0800

    add log4j dependency

--- src/main/java/com/infosky/yjj/model/Speaker.java
+++ src/main/java/com/infosky/yjj/model/Speaker.java
@@ -5,11 +5,15 @@ import org.springframework.cache.annotation.Cacheable;
 public class Speaker {
 	@Cacheable(value = "wordCache", key = "#word")
 	public String sayWord(String word) {
+		// 第一次打开 /speaker 会执行 该方法 输出
+		// 第二次开始 将直接从缓存 读取 不会执行该方法
+		System.out.println("[sayWord] method is invoked!");
 		return word;
 	}
 
 	@CacheEvict(value = "wordCache", key = "#word")
 	public String clearWord(String word) {
+		System.out.println("[clearWord] method is invoked!");
 		return "Ok";
 	}
 }
